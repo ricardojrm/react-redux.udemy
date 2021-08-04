9c707bd66c0408c804fa360710145e7dcfe19927
ricardo.ramalho <ricardo.ramalho@dataprev.gov.br>
2021-08-03 23:11:44 -0300

    Aula 23: Componente Card #02

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import React from 'react';
 import PrimeiroComponente from './components/basics/PrimeiroComponente'
 import ComponenteFragmentado from './components/basics/ComponenteComFragmento'
@@ -10,25 +11,27 @@ export default function App( props )
     return (
         <div id="app">
             <h1>Fundamentos React</h1>
-            <UICard titulo="Número Aleatório">
-                <NumeroAleatorio min={2} max={10} />
-            </UICard>
-            <UICard titulo="Componente Parametrizado">
-                <ComponenteParametrizado
-                    titulo="Situação do aluno"
-                    aluno="Pedro"
-                    nota={6.7} />
-                <ComponenteParametrizado
-                    titulo="Situação do aluno"
-                    aluno="Maria"
-                    nota={9.9} />
-            </UICard>
-            <UICard titulo="Componente Fragmentado">
-                <ComponenteFragmentado />
-            </UICard>
-            <UICard titulo="Primeiro Componente">
-                <PrimeiroComponente></PrimeiroComponente>
-            </UICard>
+            <div id="cards">
+                <UICard titulo="Número Aleatório">
+                    <NumeroAleatorio min={2} max={10} />
+                </UICard>
+                <UICard titulo="Componente Parametrizado">
+                    <ComponenteParametrizado
+                        titulo="Situação do aluno"
+                        aluno="Pedro"
+                        nota={6.7} />
+                    <ComponenteParametrizado
+                        titulo="Situação do aluno"
+                        aluno="Maria"
+                        nota={9.9} />
+                </UICard>
+                <UICard titulo="Componente Fragmentado">
+                    <ComponenteFragmentado />
+                </UICard>
+                <UICard titulo="Primeiro Componente">
+                    <PrimeiroComponente></PrimeiroComponente>
+                </UICard>
+            </div>
         </div>
     );
 }
