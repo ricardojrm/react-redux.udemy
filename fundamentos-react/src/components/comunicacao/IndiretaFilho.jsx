5139d9829d6358d79651ad1af577dd842c5a1a95
ricardo.ramalho <ricardo.ramalho@dataprev.gov.br>
2021-08-11 17:56:58 -0300

    Aula 35: Componente com Estado

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 export default function IndiretaFilho( {callback} )
 {
-    const cb = function ( e ) { callback( 'José Ricardo', 65, false ); };
+    const min = 50;
+    const max = 70;
+
+    function gerarIdade()
+    {
+        return parseInt( Math.random() * (max - min) ) + min;
+    }
+
+    function gerarNerd()
+    {
+        return Math.random() > 0.5;
+    }
+
+    const cb = function ( e ) { callback( 'José Ricardo', gerarIdade(), gerarNerd() ); };
 
     return (
         <div>
--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IndiretaFilho from './IndiretaFilho';
 
 export default function IndiretaPai()
 {
-    let nome = "?";
-    let idade = 0;
-    let nerd = false;
+    const [nome, setNome] = useState( '?' );
+    const [idade, setIdade] = useState( 0 );
+    const [nerd, setNerd] = useState( false );
 
     function preencherComponente( nome, idade, nerd )
     {
         console.log( nome, idade, nerd );
+        
+        setNome( nome );
+        setIdade( idade );
+        setNerd( nerd );
     }
 
     return (
