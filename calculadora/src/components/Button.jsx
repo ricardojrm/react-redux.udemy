0c647051b3e02abb5a2e8ecd042f53ef78671b34
ricardo.ramalho <ricardo.ramalho@dataprev.gov.br>
2021-08-30 11:41:04 -0300

    Aula 47: Componente Button #02

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
 import './Button.css';
+import React from 'react';
 
 function Button( props )
 {
+    const classNames = [ "button" ];
+    const operation = props.operation || false;
+    const double = props.double || false;
+    const triple = props.triple || false;
+    
+    if ( operation ) { classNames.push( "operation" ); }
+    if ( double ) { classNames.push( "double" ); }
+    if ( triple ) { classNames.push( "triple" ); }
+
+    function doClick( e )
+    {
+        props.click && props.click( props.label );
+    }
+
     return (
-        <button className="button">{props.label}</button>  
+        <button className={classNames.join( " " )} onClick={doClick}>{props.label}</button>  
     );
 }
 
