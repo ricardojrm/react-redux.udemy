c7330fa1c7ff2d2d2be7dba6b6d0bcd5ddb8d1d4
ricardo.ramalho <ricardo.ramalho@dataprev.gov.br>
2021-08-31 00:54:50 -0300

    Aula 48: Implementando a Lógica #01

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -5,9 +5,20 @@ import Button from '../components/Button';
 
 class Calculator extends Component
 {
+    initialState = {
+        displayValue: '0',
+        clearDisplay: false,
+        operation: null,
+        values: [ 0, 0 ],
+        current: 0,
+    }
+
+    state = {...this.initialState}
+
     clear = () =>
     {
         console.log( 'limpar' );
+        this.setState( {...this.initialState} );
     }
 
     setOperation = ( n ) =>
@@ -18,13 +29,36 @@ class Calculator extends Component
     addDigit = ( n ) =>
     {
         console.log( n );
+
+        // Validação #1: Não podemos digitar mais de '.'
+        if ( n === '.' 
+                && this.state.displayValue.includes( n ) )
+        {
+            return;
+        }
+
+        const isClearDisplay = this.state.displayValue === "0" || this.state.clearDisplay;
+        const currentValue = isClearDisplay ? '' : this.state.displayValue;
+        const displayValue = currentValue + n;
+        this.setState( {displayValue, clearDisplay: false} );
+
+        // Se o valor informado for um dígito...
+        if ( n !== "." )
+        {
+            const i = this.state.current;
+            const newValue = parseFloat( displayValue );
+            const values = this.state.values;
+            values[i] = newValue;
+            this.setState( {values} );
+            console.log( values );
+        }
     }
      
     render()
     {
         return (
             <div className="calculator">
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clear} triple />
                 <Button label="/"  click={this.setOperation} operation />
                 <Button label="7"  click={this.addDigit} />
